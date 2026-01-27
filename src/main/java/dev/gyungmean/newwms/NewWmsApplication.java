dc5bfb8d4b361cc91a98237e71753de0a1b569dc
gyungmin <miniikim0728@gmail.com>
2026-01-27 17:10:00 +0900

    ✨Create project

---
+++ src/main/java/dev/gyungmean/newwms/NewWmsApplication.java
@@ -0,0 +1,13 @@
+package dev.gyungmean.newwms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewWmsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NewWmsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/gyungmean/newwms/NewWmsApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.gyungmean.newwms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewWmsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
